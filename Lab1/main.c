2369a675773ec39c5832c1e9f9046912c88ff6b2
Louai Zahran <louai0nasr@gmail.com>
2022-03-24 00:36:19 +0200

    Export using setenv

--- Lab1/main.c
+++ Lab1/main.c
@@ -3,28 +3,21 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 #include <string.h>
-#include <malloc.h>
 #include <stdlib.h>
+#include "list.c"
 
-int pids[10000];
-bool reaped[10000];
-int pidsCount;
+#define MAX_COMMAND_SIZE 1000
+#define MAX_PROCESSES_NUM 1000
 
-//function parent_main()
-//  register_child_signal(on_child_exit())
-//  setup_environment()
-//  shell()
-//
-
-
-
-//function on_child_exit()
-//  reap_child_zombie()
-//  write_to_log_file("Child terminated")
+int pids[MAX_PROCESSES_NUM];
+bool reaped[MAX_PROCESSES_NUM];
+int pidsCount = 0;
+struct list* list;
+FILE* logfile;
 
 void write_to_log_file(int pid){
-    printf("Child process [%d] has terminated\n", pid);
-    fflush(stdin);
+    fprintf(logfile, "Child process [%d] has terminated\n", pid);
+    fflush(logfile);
 }
 
 void reap_child_zombie(){
@@ -40,11 +33,6 @@ void on_child_exit(int sig){
     reap_child_zombie();
 }
 
-//
-//function setup_environment()
-//  cd(Current_Working_Directory)
-//
-
 void printWorkingDirectory(){
     char directory[1000];
     getcwd(directory, 1000);
@@ -52,32 +40,26 @@ void printWorkingDirectory(){
 }
 
 void setup_environment(){
-    char directory[1000];
+    list = createList();
+
+    char* directory = (char*) malloc(1000 * sizeof(char));
+    char* logfileDirectory = (char*) malloc(1010 * sizeof(char));
     getcwd(directory, 1000);
     chdir(directory);
-}
+    strcpy(logfileDirectory, directory);
+    strcat(logfileDirectory, "/logs.txt");
+    logfile = fopen(logfileDirectory, "w+");
 
-//function shell()
-//  do
-//      parse_input(read_input())
-//      evaluate_expression():
-//      switch(input_type):
-//          case shell_builtin:
-//              execute_shell_builtin();
-//          case executable_or_error:
-//              execute_command():
-//
-//  while command_is_not_exit
-//
+    free(directory);
+    free(logfileDirectory);
+}
 
 //Returns the number of characters processed
 int getFirstWord(char* src, char* dst){
     char* srcStart = src, *dstStart = dst;
-    while(*src == ' '){
+    while(*src == ' ' || *src == '\t')
         src++;
-        continue;
-    }
-    while(*src != ' ' && *src != '\0')
+    while(*src != ' ' && *src != '\t' && *src != '\0')
         *dst = *src, dst++, src++;
     *dst = '\0';
     return src - srcStart;
@@ -92,34 +74,47 @@ void tokenize(char* src, char** dst){
     tokenize(src + length, dst+1);
 }
 
-//function execute_shell_builtin()
-//  switch(command_type):
-//      case cd:
-//      case echo:
-//      case export:
-//
+void replaceVariables(char** src){
+    int ptr = 0;
+    while(src[ptr] != NULL && src[ptr][0] != '\0'){
+        if(src[ptr][0] == '$'){
+            char* name = strtok(src[ptr], "$");
+            printf("%s %s\n", src[ptr], name);
+            char* value = getenv(name);//getFromList(list, src[ptr]);
+            strcpy(src[ptr], value);
+        }
+        ptr++;
+    }
+}
 
 void execute_shell_builtin(char* firstWord, char* command){
     int length = (int)strlen(command);
     if(strcmp(firstWord, "cd") == 0){
+        if(strlen(command) == 2)    //cd command without a directory
+            return;
         char* directory = malloc(1000);
-        strncpy(directory, command + 3, length - 3);
+        strncpy(directory, command + 3, length - 3);    //Skip the cd and the space after it
         chdir(directory);
+        free(directory);
     }else if(strcmp(firstWord, "echo") == 0){
-        printf("ECHO: %s", command + 4);
+        char** tokens = (char**) malloc(10 * sizeof(char*));
+        for(int i=0; i<10; i++)
+            tokens[i] = malloc(100 * sizeof(char));
+        tokenize(command, tokens);
+        replaceVariables(tokens);
+        for(int i=1; i<10; i++)
+            printf("%s ", tokens[i]);
+        printf("\n");
+    }else if(strcmp(firstWord, "export") == 0){
+        char* name = (char*) malloc(100);
+        char* value = (char*) malloc(100);
+        sscanf(command, "export %[^=]\"%[^\"]", name, value);
+        setenv(name, value, true);
+        printf("%s = %s", name, value);
     }else{
-        exit(1);
+        printf("Unsupported Command\n");
     }
 }
-
-//function execute_command()
-//  child_id = fork()
-//  if child:
-//      execvp(command parsed)
-//      print("Error)
-//      exit()
-//  else if parent and foreground:
-//      waitpid(child)
 
 void execute_command(char* firstWord, char* command, bool background){
     int pid = fork();
@@ -128,15 +123,14 @@ void execute_command(char* firstWord, char* command, bool background){
         for(int i=0; i<10; i++)
             tokens[i] = malloc(100 * sizeof(char));
         tokenize(command, tokens);
+        replaceVariables(tokens);
 
-        char *path = getenv("PATH");
-        char pathenv[strlen(path) + sizeof("PATH=")];
-        sprintf(pathenv, "PATH=%s", path);
-        char *env[] = {pathenv, "x=-l", NULL};
-        printf("%s\n", pathenv);
-        execve("/bin/ls", tokens, env);
-        printf("ERRORRR!!!\n");
-        exit(2);
+        execvp(firstWord, tokens);
+        printf("Unsupported Command\n");
+        for(int i=0; i<10; i++)
+            free(tokens[i]);
+        free(tokens);
+        exit(1);
     }else{  //Parent
         pids[pidsCount++] = pid;
         if(!background)
@@ -146,19 +140,28 @@ void execute_command(char* firstWord, char* command, bool background){
 
 void shell(){
     while(true){
-        bool bg = false;
-        char command[1000];
+        bool bg = false;                                   //Assumption until proven otherwise
+        char* command = malloc(MAX_COMMAND_SIZE);
+        char* commandStart = command;
+
         printWorkingDirectory();
+
         fgets(command, 1000, stdin);
-        command[strlen(command) - 1] = '\0'; //To ignore the newline character from fgets
+        command[strlen(command) - 1] = '\0';            //To ignore the newline character from fgets
+
+        while(command[0] == ' ' || command[0] == '\t')     //To ignore leading spaces
+            command++;
+
         if(command[strlen(command) - 1] == '&'){
-            command[strlen(command) - 1] = '\0';
+            command[strlen(command) - 1] = '\0';        //We don't need the '&' anymore
             bg = true;
-            while(command[strlen(command) - 1] == ' ')
-                command[strlen(command) - 1] = '\0';
+            while(command[strlen(command) - 1] == ' ' || command[strlen(command) - 1] == '\t')
+                command[strlen(command) - 1] = '\0';    //To ignore trailing spaces
         }
-        if(strlen(command) == 0)
+
+        if(strlen(command) == 0)                        //Nothing to execute
             continue;
+
         char firstWord[20];
         getFirstWord(command, firstWord);
         if(strcmp(firstWord, "exit") == 0)
@@ -169,6 +172,8 @@ void shell(){
             execute_shell_builtin(firstWord, command);
         else
             execute_command(firstWord, command, bg);
+
+        free(commandStart); //To free the whole allocated array from the memory, even with ignored spaces
     }
 }
 
@@ -176,11 +181,6 @@ int main() {
     signal(SIGCHLD, &on_child_exit);
     setup_environment();
     shell();
-
-//    char** buffer = (char**) malloc(10 * sizeof(char*));
-//    for(int i=0; i<10; i++)
-//        buffer[i] = malloc(100 * sizeof(char));
-//    tokenize("HELLO, WORLD! I'm Louai", buffer);
 
     return 0;
 }
