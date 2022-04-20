d3b02ef8199faac17b55b5c0e1f25a977b41b449
Louai Zahran <louai0nasr@gmail.com>
2022-04-20 22:43:47 +0200

    Commented Lab3

--- Lab3/caltrain.c
+++ Lab3/caltrain.c
@@ -1,11 +1,9 @@
 #include <pthread.h>
-#include <stdio.h>
 #include "caltrain.h"
 
 void
 station_init(struct station *station)
 {
-    // FILL ME IN
     station->availableSeats = 0;
     station->waitingPassengers = 0;
     station->boardingPassengers = 0;
@@ -17,15 +15,14 @@ station_init(struct station *station)
 void
 station_load_train(struct station *station, int count)
 {
-    // FILL ME IN
     pthread_mutex_lock(&(station->mutex));
-    if(count == 0 || station->waitingPassengers == 0) {
+    if(count == 0 || station->waitingPassengers == 0) { //Nothing to do, return immediately
         pthread_mutex_unlock(&(station->mutex));
         return;
     }
     station->availableSeats = count;
-    pthread_cond_broadcast(&(station->trainArrival));
-    pthread_cond_wait(&(station->boardingDone), &(station->mutex));
+    pthread_cond_broadcast(&(station->trainArrival));   //Signal for all passengers to try to board
+    pthread_cond_wait(&(station->boardingDone), &(station->mutex)); //Wait until all of them are boarded before it departs
     station->availableSeats = 0;
     pthread_mutex_unlock(&(station->mutex));
 }
@@ -33,10 +30,9 @@ station_load_train(struct station *station, int count)
 void
 station_wait_for_train(struct station *station)
 {
-    // FILL ME IN
     pthread_mutex_lock(&(station->mutex));
     station->waitingPassengers++;
-    while(station->availableSeats == 0)
+    while(station->availableSeats == 0) //"While" instead of "if" due to passengers racing for the available seats
         pthread_cond_wait(&(station->trainArrival), &(station->mutex));
     station->availableSeats--;
     station->waitingPassengers--;
@@ -47,7 +43,6 @@ station_wait_for_train(struct station *station)
 void
 station_on_board(struct station *station)
 {
-    // FILL ME IN
     pthread_mutex_lock(&(station->mutex));
     station->boardingPassengers--;
     if(station->boardingPassengers == 0 && (station->availableSeats == 0 || station->waitingPassengers == 0))
--- Lab3/caltrain.h
+++ Lab3/caltrain.h
@@ -7,18 +7,23 @@
 
 struct station {
     // FILL ME IN
-    int availableSeats;
-    int waitingPassengers;
-    int boardingPassengers;
-    pthread_mutex_t mutex;
-    pthread_cond_t trainArrival;
-    pthread_cond_t boardingDone;
+    int availableSeats;     //Number of free seats on the train, defaults to zero of no train arrived
+    int waitingPassengers;  //Number of passengers waiting for a train with free seats to arrive
+    int boardingPassengers; //Number of passengers who found free seats and are currently boarding
+    pthread_mutex_t mutex;  //For synchronization
+    pthread_cond_t trainArrival;  //A flag for when a new train arrives so that waiting passenger should search for free seats
+    pthread_cond_t boardingDone;  //A flag for when a train is fully boarded and ready to depart
 };
 
+//Initializer function
 void station_init(struct station *station);
 
+//Called when a new train arrives at the station with "count" be the number of free seats on the train
+//Returns when all passengers board, or no more free seats are found, or no more passengers are waiting for the train
 void station_load_train(struct station *station, int count);
 
+//Called when a passenger is waiting for a train with available free seats
 void station_wait_for_train(struct station *station);
 
+//Called when a passenger found a free seat and is currently boarding
 void station_on_board(struct station *station);
